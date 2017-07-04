6efebe861ee33b6e33b955f49af57198f2076f5c
kinggit918 <410727984@qq.com>
2017-07-04 18:15:54 +0800

    Update basic_image.sh

--- centos/basic_image.sh
+++ centos/basic_image.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+yum -y install febootstrap
+febootstrap -i bash -i wget -i yum -i iputils -i iproute centos6 centos6-doc http://mirrors.163.com/centos/6/os/x86_64/
+cd centos6-doc/
+tar -c .|docker import - centos
